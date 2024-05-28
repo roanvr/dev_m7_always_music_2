45f4abee8bd9ae5523b7fee725b133fa40e2384e
RodrigoValdivia <rodrigo.valdivia@usach.cl>
2024-05-28 00:41:39 -0400

    función agregar estudiante lista :check:

--- queries/consultas.js
+++ queries/consultas.js
@@ -1,15 +1,40 @@
 import pool from '../config/db.js';
+const argumento = process.argv.slice(2);
+const opcion = argumento[0];
+const genero = argumento[1];
+const fdn = argumento[2];
+const telefono = argumento[3];
+const email = argumento[4];
+const pais = argumento[5];
+const nombre = argumento[6];
 
 const agregarEstudiante = async(genero, fdn, telefono, email, pais, nombre) => {
     try {
         const consulta = {
-            sql: 'INSERT INTO estudiantes (genero, fdn,telefono, email, pais, nombre) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+            text: 'INSERT INTO estudiantes (genero, fdn,telefono, email, pais, nombre) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
             values: [genero, fdn, telefono, email, pais, nombre]
-        }
+        };
         
         const response = await pool.query(consulta);
         console.log('El estudiante fue agregado exitosamente', response.rows[0]);
     } catch (error){
         console.log(error.code, error.message);
     }
+};
+
+agregarEstudiante(genero, fdn, telefono, email, pais, nombre);
+
+
+const mostrarEstudiantes = async () => {
+    try {
+        const consulta = {
+            text: 'SELECT * FROM estudiantes',
+        };
+        const response = await pool.query(consulta);
+        console.log('El registro actual de estudiantes es:', response.rows);
+    } catch (error) {
+        console.log(error.code, error.message)
+    };
 }
+
+mostrarEstudiantes();
